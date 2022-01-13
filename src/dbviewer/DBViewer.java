6211296e683985e840ef105282da1c539433f265
Andreas Wegner <andreaswegner@posteo.de>
2022-01-13 15:50:07 +0100

    Set up project structure

---
+++ src/dbviewer/DBViewer.java
@@ -0,0 +1,19 @@
+package dbviewer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DBViewer extends Application
+{
+	public static void main(String[] args)
+	{
+		Application.launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage)
+	{
+		stage.setTitle("MegaStarshipsGame 2.0 2020 HD-Remaster - Database Viewer");
+		stage.show();
+	}
+}
