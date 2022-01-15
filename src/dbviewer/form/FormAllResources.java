475b53ba3561193e3e9129cdb76e16185319f38b
Andreas Wegner <andreaswegner@posteo.de>
2022-01-15 13:12:09 +0100

    Added base class for forms that only consists of a single table and display all data of the result

--- src/dbviewer/form/FormAllResources.java
+++ src/dbviewer/form/FormAllResources.java
@@ -1,40 +1,10 @@
 package dbviewer.form;
 
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ResourceBundle;
-
-import dbviewer.DatabaseHelper;
-import dbviewer.DatabaseHelper.StringArrayTableRow;
-import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
-
-public class FormAllResources extends Form
+public class FormAllResources extends FormSingleTable
 {
-	@FXML
-	private TableView<StringArrayTableRow> entries;
-	
 	@Override
-	public String getFXMLPath()
+	public String getQueryString()
 	{
-		return "res/forms/all_resources.fxml";
-	}
-
-	@Override
-	public void executeQuery(Connection conn) throws SQLException
-	{
-		try(Statement st = conn.createStatement())
-		{
-			if(st.execute("SELECT resource_name AS \"Resource\" FROM PLANET_RESOURCE"))
-			{
-				DatabaseHelper.fillFXTable(this.entries, st.getResultSet());
-			}
-		}
-		catch(SQLException e)
-		{
-			throw e;
-		}
+		return "SELECT resource_name AS \"Resource\" FROM PLANET_RESOURCE";
 	}
 }
---
+++ src/dbviewer/form/FormSingleTable.java
@@ -0,0 +1,40 @@
+package dbviewer.form;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import dbviewer.DatabaseHelper;
+import dbviewer.DatabaseHelper.StringArrayTableRow;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public abstract class FormSingleTable extends Form
+{
+	@FXML
+	private TableView<StringArrayTableRow> entries;
+	
+	@Override
+	public final String getFXMLPath()
+	{
+		return "res/forms/single_table.fxml";
+	}
+
+	@Override
+	public final void executeQuery(Connection conn) throws SQLException
+	{
+		try(Statement st = conn.createStatement())
+		{
+			if(st.execute(this.getQueryString()))
+			{
+				DatabaseHelper.fillFXTable(this.entries, st.getResultSet());
+			}
+		}
+		catch(SQLException e)
+		{
+			throw e;
+		}
+	}
+	
+	public abstract String getQueryString();
+}
