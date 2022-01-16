43a40bd8c3ae4a234ec4e56d28f353abb7f3c71e
Andreas Wegner <andreaswegner@posteo.de>
2022-01-16 12:16:31 +0100

    Removed FormAllAccounts example

--- src/dbviewer/DBViewer.java
+++ src/dbviewer/DBViewer.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 import dbviewer.form.FormAllAccountTiers;
-import dbviewer.form.FormAllAccounts;
 import dbviewer.form.FormAllBuildingTypes;
 import dbviewer.form.FormAllCountries;
 import dbviewer.form.FormAllOffers;
@@ -86,7 +85,6 @@ public class DBViewer extends Application
 	
 	private void loadForms()
 	{
-		this.formsHandler.registerForm(FormAllAccounts.class, "Get all accounts");
 		this.formsHandler.registerForm(FormAllAccountTiers.class, "Get all account tiers");
 		this.formsHandler.registerForm(FormAllBuildingTypes.class, "Get all building types");
 		this.formsHandler.registerForm(FormAllCountries.class, "Get all countries");
